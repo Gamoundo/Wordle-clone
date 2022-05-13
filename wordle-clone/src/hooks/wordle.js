ab6ce2f4bf1893df16239a8cf522936ac215768c
Akil Griffiths <akilgg@gmail.com>
2022-05-13 14:40:31 -0400

    added wordle logic comp

---
+++ wordle-clone/src/hooks/wordle.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+const wordle = (solution) => {
+
+const [turn, setTurn] = useState(0)
+const [currentGuess, setCurrentGuess] = useState('')
+const [guesses, setGuesses] = useState([])
+const [history, setHistory] = useState([])
+const [correct, setCorrect] = useState(false)
+
+
+
+
+
+const formatGuess = () => {
+
+}
+
+const addGuess = () => {
+
+}
+
+const key = () => {
+
+}
+
+return {turn, currentGuess, guesses, correct, key}
+
+}
+
+export default wordle
