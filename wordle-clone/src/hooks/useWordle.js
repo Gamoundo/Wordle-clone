ed6dcec5f2051e83830f32272cd821a662b0bd7e
Akil Griffiths <akilgg@gmail.com>
2022-05-13 16:44:36 -0400

    Added enter key logic

--- wordle-clone/src/hooks/useWordle.js
+++ wordle-clone/src/hooks/useWordle.js
@@ -13,7 +13,7 @@ const [correct, setCorrect] = useState(false)
 
 
 const formatGuess = () => {
-
+console.log("formatting the guess", currentGuess)
 }
 
 const addGuess = () => {
@@ -21,6 +21,33 @@ const addGuess = () => {
 }
 
 const handleKeyUp = ({key}) => {
+if (key === 'Enter') {
+    
+    if (turn > 5) {
+        console.log("Guess time is over")
+        return
+    }
+
+    if (history.includes(currentGuess)) {
+        console.log("there are other words")
+        return
+    }
+
+    if (currentGuess.length < 5) {
+        console.log("Five. Letter. Words.")
+        return
+    }
+
+    formatGuess()
+}
+
+
+    if (key === 'Backspace') {
+        setCurrentGuess((prev) => {
+            return prev.slice(0, -1)
+        })
+        return
+    }
 if (/^[A-Za-z]$/.test(key)) {
     if(currentGuess.length < 5) {
         setCurrentGuess((prev) => {
