b784ae99a67811fa97104769c00ea21d41d04627
Akil Griffiths <akilgg@gmail.com>
2022-05-14 20:45:03 -0400

    grid is color coded now
    it takes the guess and based on accuracy color codes the word

--- wordle-clone/src/components/Row.js
+++ wordle-clone/src/components/Row.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
-export default function Row() {
-  return (
-    <div className='row'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-  )
+export default function Row({guess}) {
+  
+    if (guess) {
+        return (
+            <div className='row'>
+               {guess.map((l, i) => {
+                  return <div key={i} className={l.color}> {l.key}</div>
+               })}
+           </div>
+         )  
+    }
+    
+    return (
+        <div className='row'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
 }
